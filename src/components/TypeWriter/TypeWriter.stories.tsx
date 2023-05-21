6c33b8221a68507474c08042d6146bfd1f3bfb9c
Junn <socker210@gmail.com>
2023-05-21 17:23:18 +0900

    feat(component): add TypeWriter component

---
+++ src/components/TypeWriter/TypeWriter.stories.tsx
@@ -0,0 +1,34 @@
+import { Meta, StoryObj } from '@storybook/react'
+import TypeWriter from './TypeWriter'
+
+type Story = StoryObj<typeof TypeWriter>
+
+const meta: Meta<typeof TypeWriter> = {
+  title: 'TypeWriter',
+  component: TypeWriter,
+}
+
+export const Primary: Story = {
+  render: () => (
+    <TypeWriter>
+      <h1>HAHA1</h1>
+      <h1>HAHA2</h1>
+    </TypeWriter>
+  ),
+}
+
+export const WithComponent: Story = {
+  render: () => {
+    const Comp: React.FC = () => {
+      return <h1 className='text-red-400'>Component</h1>
+    }
+
+    return (
+      <TypeWriter>
+        <Comp />
+      </TypeWriter>
+    )
+  },
+}
+
+export default meta
