832486024c7212c0cab46f42e9c8a923b30735fb
Junn <socker210@gmail.com>
2023-05-25 19:04:08 +0900

    chore: test layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,21 @@
-import type { ReactElement } from 'react'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
 import type { NextPageWithLayout } from './_app'
 
 const Page: NextPageWithLayout = () => {
-  return null
-}
-
-Page.getLayout = function getLayout(page: ReactElement) {
-  return <>{page}</>
+  return (
+    <div>
+      <Header />
+      <div tw='h-screen p-pc-spacing'>
+        <p tw='text-2xl'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat quo
+          quam perspiciatis nisi, ad, harum et tenetur mollitia pariatur dolor
+          maxime esse beatae fugiat alias nobis tempore, ipsam vel inventore!
+        </p>
+      </div>
+      <Footer />
+    </div>
+  )
 }
 
 export default Page
