6d444030446dde0eaa5add96f6612230a474552b
Junn <socker210@gmail.com>
2023-06-01 00:30:24 +0900

    style(page): refactor about-me page layout style

--- styles/snippets.tsx
+++ styles/snippets.tsx
@@ -13,8 +13,3 @@ export const Vars = styled.div`
 `
 
 export const Block = styled.div(() => [tw`mx-auto max-w-7xl`])
-
-export const ContentBox = styled.main(() => [
-  tw`min-h-[calc(100vh-theme('spacing.m-header'))] p-m-spacing`,
-  tw`sm:min-h-[calc(100vh - theme('spacing.pc-header'))] sm:p-pc-spacing`,
-])
